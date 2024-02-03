cee968731d5f86d9c30fecff5564f5fc57b06a72
Isela Soria <rosaiselasoriamonzon@gmail.com>
2024-02-03 01:16:40 -0800

    exercises in prep

---
+++ exercises/py-prep/data_types_exercises.py
@@ -0,0 +1,39 @@
+# 1 Identify the data type or class for each of the following:
+# Value
+'True'          # String
+False           # Bool
+(1, 2, 3)       # tuple
+1.5             # float
+[1, 2, 3]       # list
+2               # integer
+range(5)        # range
+{1, 2, 3}       # set
+None            # NoneType
+{'foo': 'bar'}  # dictionary
+
+# 2 Create a tuple called names that contains several pet names. For instance:
+# Name
+# Asta
+# Butterscotch
+# Pudding
+# Neptune
+# Darwin
+
+tup = ('Burbujita','Gotita','Espumita')
+
+# 3 Create a dictionary named pets that contains a list of 
+# pet names and the type of animal. For instance:
+
+
+# Name	        Animal
+# Asta	        dog
+# Butterscotch	cat
+# Pudding	    cat
+# Neptune	    fish
+# Darwin	    lizard
+
+pets = {
+    'Burbus':'cat',
+    'Charlie':'dog',
+    'Lady Blue': 'fish'
+}
