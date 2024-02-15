afc71c56430b162aa5a8dd2b6dbbc8a18a79109f
Isela Soria <rosaiselasoriamonzon@gmail.com>
2024-02-14 22:11:08 -0800

    commiting the deletion of the files from previous dir

--- exercises/py-prep/py100-python-basics/logical_condition2.py
+++
@@ -1,9 +0,0 @@
-# predict the outcome:
-if True and False: 
-    print('Yes!')
-else:
-    print('No...') 
-    #### this will always be 'No' because using and means that since we have a False in the condition, the
-    # whole thing will not evaluate to a truthy value and therefore that block of code will never execute
-
-
