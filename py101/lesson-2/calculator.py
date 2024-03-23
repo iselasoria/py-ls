24d088a5bb1f74690cecb8a4e0a1acc82f6220ce
Isela Soria <rosaiselasoriamonzon@gmail.com>
2024-03-22 18:19:16 -0700

    Pft- validating user input

--- py101/lesson-2/calculator.py
+++ py101/lesson-2/calculator.py
@@ -3,6 +3,13 @@ import time
 def prompt(message):
     print(f'==> {message}')
 
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
 
 """
 Ask the user for the first number.
@@ -19,11 +26,24 @@ print()
 prompt("What's the first number?")
 number1 = input()
 
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+
 prompt("What is the second number?")
 number2 = input()
 
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
+
 prompt('What operation do you wan to perform? \n1)Add 2)Subtract 3)Multiply 4)Divide')
 operation = input()
+
+while operation not in ["1", "2", "3", "4"]:
+    prompt('You must choose 1, 2, 3, or 4')
+    operation = input()
 
 match operation:
     case '1':
