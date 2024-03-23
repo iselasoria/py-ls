0d7544e094a4bb02889fde5501e19afdbaa090c5
Isela Soria <rosaiselasoriamonzon@gmail.com>
2024-03-22 18:08:46 -0700

    replacing if/else with the match case statement

--- py101/lesson-2/calculator.py
+++ py101/lesson-2/calculator.py
@@ -11,7 +11,7 @@ Ask the user for an operation to perform.
 Perform the operation on the two numbers.
 Print the result to the terminal
 """
-prompt('Welcome to Calculator!')
+print('Welcome to Calculator!')
 
 time.sleep(2)
 print()
@@ -25,13 +25,14 @@ number2 = input()
 prompt('What operation do you wan to perform? \n1)Add 2)Subtract 3)Multiply 4)Divide')
 operation = input()
 
-if operation == '1':
-    output = int(number1) + int(number2)
-elif operation == '2':
-    output = int(number1) - int(number2)
-elif operation == '3':
-    output = int(number1) * int(number2)
-elif operation == '4':
-    output = int(number1) // int(number2)
+match operation:
+    case '1':
+        output = int(number1) + int(number2)
+    case '2':
+        output = int(number1) - int(number2)
+    case '3':
+        output = int(number1) * int(number2)
+    case '4':
+        output = int(number1) // int(number2)
 
 print(f'The result is {output}')
