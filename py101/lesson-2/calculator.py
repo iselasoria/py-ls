c9cdad4f47de68ea01b533143e28eaf20a3ec0a2
Isela Soria <rosaiselasoriamonzon@gmail.com>
2024-03-22 17:57:16 -0700

    adding prompt

--- py101/lesson-2/calculator.py
+++ py101/lesson-2/calculator.py
@@ -1,4 +1,9 @@
 import time 
+
+def prompt(message):
+    print(f'==> {message}')
+
+
 """
 Ask the user for the first number.
 Ask the user for the second number.
@@ -6,18 +11,18 @@ Ask the user for an operation to perform.
 Perform the operation on the two numbers.
 Print the result to the terminal
 """
-print('Welcome to Calculator!')
+prompt('Welcome to Calculator!')
 
 time.sleep(2)
 print()
 # Ask the user for the first number
-print("What's the first number?")
+prompt("What's the first number?")
 number1 = input()
 
-print("What is the second number?")
+prompt("What is the second number?")
 number2 = input()
 
-print('What operation do you wan to perform? \n1)Add 2)Subtract 3)Multiply 4)Divide')
+prompt('What operation do you wan to perform? \n1)Add 2)Subtract 3)Multiply 4)Divide')
 operation = input()
 
 if operation == '1':
