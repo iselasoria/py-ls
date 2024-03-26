1b1620261a51e692fee22dfa51db6da8249f81d7
Isela Soria <rosaiselasoriamonzon@gmail.com>
2024-03-25 23:52:26 -0700

    Pft- HEAD of the file was problematic, need to find out why it because embedded in the file

--- py101/lesson-2/calculator.py
+++ py101/lesson-2/calculator.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-
-"""
-Ask the user for the first number.
-Ask the user for the second number.
-Ask the user for an operation to perform.
-Perform the operation on the two numbers.
-Print the result to the terminal
-"""
-
-=======
->>>>>>> FT-refactor_calc
 import time
 
 def prompt(message):
