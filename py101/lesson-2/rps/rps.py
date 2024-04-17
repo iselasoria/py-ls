28fd1e5fb5ac9fddc96b1c5d37bea09b8eb8177e
Xela <75233552+iselasoria@users.noreply.github.com>
2024-04-16 19:22:46 -0700

    rps
    P- rps cleaning logic to new functions, adding validation and looping
    mechanism

--- py101/lesson-2/rps/rps.py
+++ py101/lesson-2/rps/rps.py
@@ -4,27 +4,45 @@ VALID_CHOICES = ['rock','paper','scissors']
 
 def prompt(msg):
     print(f'==> {msg}')
+    
+def display_winner(player, computer):
+    if ((player == 'rock' and computer == 'scissors') or
+        (player == 'paper' and computer == 'rock') or
+        (player == 'scissors' and computer == 'paper')):
+        prompt('You win!')
+    elif ((player == 'rock' and computer == 'paper') or
+          (player == 'paper' and computer == 'scissors') or
+          (player == 'scissors' and computer == 'rock')):
+        prompt('Computer wins!')
+    else:
+        prompt('It\'s a tie!')
+    
+    
+while True: 
 
-prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
+    prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
 
-choice = input()
-
-
-while choice not in VALID_CHOICES:
-    prompt('That\'s not a valid choice')
     choice = input()
 
-computer_choice = random.choice(VALID_CHOICES)
 
-prompt(f'You chose: {choice}, the computer chose: {computer_choice}')
+    while choice not in VALID_CHOICES:
+        prompt('That\'s not a valid choice')
+        choice = input()
 
-if ((choice == 'rock' and computer_choice == 'scissors') or
-    (choice == 'paper' and computer_choice == 'rock') or
-    (choice == 'scissors' and computer_choice == 'paper')):
-    prompt('You win!')
-elif ((choice == 'rock' and computer_choice == 'paper') or
-      (choice == 'paper' and computer_choice == 'scissors') or
-      (choice == 'scissors' and computer_choice == 'rock')):
-    prompt('Computer wins!')
-else:
-    prompt('It\'s a tie!')
+    computer_choice = random.choice(VALID_CHOICES)
+
+    prompt(f'You chose: {choice}, the computer chose: {computer_choice}')
+
+    display_winner(choice, computer_choice)
+    
+    # break if the user doesnt want to play
+    prompt('Do you want to play again (y/n)?')
+    answer = input().lower()
+    while True:
+        if answer.startswith('n' or answer.startswith('y'):
+            break
+        prompt('Please enter "y" or "n".')
+        answer = input().lower()
+     
+    if answer[0]=='n':
+        break
