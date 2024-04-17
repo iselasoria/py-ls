0f72b04b6ef20124d482e5d826c3d63bbccb4453
Isela Soria <rosaiselasoriamonzon@gmail.com>
2024-04-16 23:08:36 -0700

    P- rps after pylint

--- py101/lesson-2/rps/rps.py
+++ py101/lesson-2/rps/rps.py
@@ -4,7 +4,7 @@ VALID_CHOICES = ['rock','paper','scissors']
 
 def prompt(msg):
     print(f'==> {msg}')
-    
+
 def display_winner(player, computer):
     if ((player == 'rock' and computer == 'scissors') or
         (player == 'paper' and computer == 'rock') or
@@ -16,9 +16,9 @@ def display_winner(player, computer):
         prompt('Computer wins!')
     else:
         prompt('It\'s a tie!')
-    
-    
-while True: 
+
+
+while True:
 
     prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
 
@@ -34,15 +34,15 @@ while True:
     prompt(f'You chose: {choice}, the computer chose: {computer_choice}')
 
     display_winner(choice, computer_choice)
-    
+
     # break if the user doesnt want to play
     prompt('Do you want to play again (y/n)?')
     answer = input().lower()
     while True:
-        if answer.startswith('n' or answer.startswith('y'):
+        if answer.startswith('n') or answer.startswith('y'):
             break
         prompt('Please enter "y" or "n".')
         answer = input().lower()
-     
+
     if answer[0]=='n':
         break
