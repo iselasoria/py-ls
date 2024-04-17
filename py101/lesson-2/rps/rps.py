f3cfb2f843551aba121dc0a38405ca3436fec442
Isela Soria <rosaiselasoriamonzon@gmail.com>
2024-04-17 16:16:20 -0700

    Pft- adding lizard and spock to game logic

--- py101/lesson-2/rps/rps.py
+++ py101/lesson-2/rps/rps.py
@@ -1,21 +1,50 @@
 import random
 
-VALID_CHOICES = ['rock','paper','scissors']
+VALID_CHOICES = ['rock','paper','scissors','lizard','spock']
+
+WINNING_MOVES = {
+    'rock': ['scissors','lizard'],
+    'paper': ['rock','spock'],
+    'scissors': ['lizard','paper'],
+    'lizard': ['paper','spock'],
+    'spock' : ['scissors','rock']
+}
 
 def prompt(msg):
     print(f'==> {msg}')
 
-def display_winner(player, computer):
-    if ((player == 'rock' and computer == 'scissors') or
-        (player == 'paper' and computer == 'rock') or
-        (player == 'scissors' and computer == 'paper')):
-        prompt('You win!')
-    elif ((player == 'rock' and computer == 'paper') or
-          (player == 'paper' and computer == 'scissors') or
-          (player == 'scissors' and computer == 'rock')):
-        prompt('Computer wins!')
+def win(first, second):
+    if second in WINNING_MOVES[first]:
+        print(WINNING_MOVES[first])
+        return True
+
+    # if the first score is included as a key, and the second is included as a value
+    # this returns true for a win
+
+def display_winner(score1, score2):
+    if win(score1, score2):
+        print('The player wins this round!')
+    elif win(score2, score1):
+        print('The computer wins this round!')
     else:
-        prompt('It\'s a tie!')
+        print('We have a tie!')
+    # if when we pass player as the first arg to _win_ we get true
+        # then display the player won
+    # otherwise if the pass the scores swapped
+        # return that the computer on
+    # otherwise, return that it is a tie
+
+# def display_winner(player, computer):
+#     if ((player == 'rock' and computer == 'scissors') or
+#         (player == 'paper' and computer == 'rock') or
+#         (player == 'scissors' and computer == 'paper')):
+#         prompt('You win!')
+#     elif ((player == 'rock' and computer == 'paper') or
+#           (player == 'paper' and computer == 'scissors') or
+#           (player == 'scissors' and computer == 'rock')):
+#         prompt('Computer wins!')
+#     else:
+#         prompt('It\'s a tie!')
 
 
 while True:
