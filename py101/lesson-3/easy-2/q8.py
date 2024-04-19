0ceebcdad8c3ffe4a1a6da9b14db5b58bbda10ca
Isela Soria <rosaiselasoriamonzon@gmail.com>
2024-04-18 23:48:02 -0700

    E- adding more questions from lesson 3

---
+++ py101/lesson-3/easy-2/q8.py
@@ -0,0 +1,8 @@
+# Determine whether the following dictionary of people and their age contains an entry for 'Spot':
+ages = {'Herman': 32, 'Lily': 30, 'Grandpa': 402, 'Eddie': 10}
+
+# retrieve the value if the key exists
+print(ages.get('Spot'))
+
+# checking if 'Spot' is there
+print('Spot' in ages)
