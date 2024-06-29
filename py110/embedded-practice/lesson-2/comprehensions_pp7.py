c0aa3d41bb3c7fcea194191139f3191b2ba646d7
Isela Soria <rosaiselasoriamonzon@gmail.com>
2024-06-29 14:56:10 -0700

    E- embedded exercises comprehensions chunk

---
+++ py110/embedded-practice/lesson-2/comprehensions_pp7.py
@@ -0,0 +1,42 @@
+"""
+Given the following data structure return a new list identical
+in structure to the original,
+but containing only the numbers that are multiples of 3.
+"""
+
+lst = [[2], [3, 5, 7, 12], [9], [11, 15, 18]]
+
+# using for loops
+new_list = []
+
+for tiny_list in lst:
+    mini_list = []
+    for num in tiny_list:
+        if num % 3 == 0:
+            mini_list.append(num)
+    new_list.append(mini_list)
+(print('This was done with loops: '))
+print(new_list)
+
+
+# refactored
+new_list = []
+
+for tiny_list in lst:
+    mini_list = [num for num in tiny_list if num % 3 == 0]
+    new_list.append(mini_list)
+print('This was done with one list comprehension')
+print(new_list)
+
+# refactor the comprehensions above into a helper function
+def multiples_of_three(tiny_list):
+    return [num for num in tiny_list if num % 3 == 0]
+
+new_list = [multiples_of_three(tiny_list) for tiny_list in lst]
+print('This was done with multiple comprehensions and a helper function: ')
+print(new_list)
+
+# expected result
+[[], [3, 12], [9], [15, 18]]
+
+# ! TODO revise; this could be the chunk!!!!
